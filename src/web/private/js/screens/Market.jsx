038b40e5e7543b46a1bb650073809007e53e79d3
caetanosasia <32081312+caetanosasia@users.noreply.github.com>
2019-06-15 18:31:22 -0300

    echarts funcionando

--- src/web/private/js/screens/Market.jsx
+++ src/web/private/js/screens/Market.jsx
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
-
-
+import echarts from 'echarts';
+1
 class Market extends React.Component{
+    componentDidMount(){
+        let myCharts = echarts.init(document.getElementById("mycharts"));
+        console.log(myCharts);
+        myCharts.setOption({
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line'
+            }]
+        });
+    }
     render(){
         return(
+            <React.Fragment>
             <h1>page Market</h1>
+                <div id="mycharts" style={{width:700,height:500}}>
+
+                </div>
+            </React.Fragment>
         )
     }
 }
