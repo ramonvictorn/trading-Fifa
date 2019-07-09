6a2ac83937caf3abefb40cb8215216b52dda1c49
caetanosasia <32081312+caetanosasia@users.noreply.github.com>
2019-07-09 00:03:27 -0300

    asd

--- src/web/private/js/components/PlayersTable.jsx
+++ src/web/private/js/components/PlayersTable.jsx
@@ -21,13 +21,7 @@ class Market extends React.Component{
         if(tela == 'mercado') {
            this.getMarketList(idPlataform, offset, qtd, firstCharge);
         } else if (tela == 'carteira'){
-            this.getWalletList(idPlataform, offset, qtd, firstCharge, (idPlataform, firstCharge)=>{if(firstCharge && idPlataform == 1) {
-                this.setState({xBoxLista:this.state.lista});
-            } else if(firstCharge && idPlataform == 2) {
-                this.setState({ps4Lista:this.state.lista});
-            } else if(firstCharge && idPlataform == 3) {
-                this.setState({pcLista:this.state.lista});
-            }})
+            this.getWalletList(idPlataform, offset, qtd, firstCharge)
         }
     }
 
@@ -63,7 +57,7 @@ class Market extends React.Component{
         });
     }
 
-
+    
     getLastPrice(player){
         let serverAns = {};
         let dados = {
@@ -101,7 +95,13 @@ class Market extends React.Component{
             complete: () => {
                 console.log('exemplo de getWallet -> ' ,this.serverAns.data)
                 this.setState({lista:this.serverAns.data});
-                console.log(this.serverAns.data)
+                if(firstCharge && idPlataform == 1) {
+                    this.setState({xBoxLista:this.state.lista});
+                } else if(firstCharge && idPlataform == 2) {
+                    this.setState({ps4Lista:this.state.lista});
+                } else if(firstCharge && idPlataform == 3) {
+                    this.setState({pcLista:this.state.lista});
+                }
             }
         });
     }
