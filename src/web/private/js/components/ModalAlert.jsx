214212764b558b30078bcbd886c30712cf76f25b
Ramon Victorn <ramonvictorn@gmail.com>
2019-07-09 16:09:55 -0300

    change loading img

--- src/web/private/js/components/ModalAlert.jsx
+++ src/web/private/js/components/ModalAlert.jsx
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faCamera , faTimesCircle} from '@fortawesome/free-solid-svg-icons';
+import Loader from 'react-loader-spinner'
 // import { timesCircle } from '@fortawesome/free-solid-svg-icons'
 class ModalAlert extends Component {
     constructor(){
         super();
     }
     render(){
-        console.log('render modal status', this.props.status)
         let modalBody = '';
         if(this.props.status == 'loading'){
-            modalBody = <img className={'imgBall'} src={'/assets/soccerball.png'}></img>
+            // modalBody = <img className={'imgBall'} src={'/assets/soccerball.png'}></img>
+            modalBody= <Loader type="Ball-Triangle" color="#663ab5" height={80} width={80} />
         }else{
             modalBody = this.props.modalBody;
         }
